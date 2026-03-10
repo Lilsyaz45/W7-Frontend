f48e7c0d6edb007c52d37b43541181a9e5565019
Lilsyaz45 <lilsyaz2004@gmail.com>
2026-03-11 00:00:46 +0700

    done

--- exercise-3/src/App.jsx
+++ exercise-3/src/App.jsx
@@ -1,25 +1,47 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
   /* You will need to use many state to keep the inut values and other needs */
+  const [a, setA] = useState("");
+  const [b, setB] = useState("");
+  const [result, setResult] = useState("");
 
   /* You will need some function to handle the key pressed and button events */
+  
+  function onA(event) {
+    setA(event.target.value);
+  
+  }
 
+  function onB(event) {
+    setB(event.target.value)
+  }
+
+  function onCompute() {
+    if (isNaN(a) || isNaN(b) || a === "" || b === "") {
+      setResult("A and B shall be number")
+    } else {
+      
+      setResult(Number(a) + Number(b))
+    }
+
+  }
   return (
     <main>
       <h1>Calculator</h1>
 
       <label>A =</label>
-      <input onKeyUp={onA} />
+      <input onChange={onA} value={a} />
 
       <label>B =</label>
-      <input onKeyUp={onB} />
+      <input onChange={onB} value={b} />
 
       <label>A + B =</label>
 
       {/* When Compute buton is clicked, this input display the sum of the 2 numbers, or the error message in RED */}
-      <input disabled />
-      <button>Compute</button>
+      <input value={result} disabled className={isNaN(result) ? "error" : ""}/>
+      <button onClick={onCompute}>Compute</button>
     </main>
   );
 }
